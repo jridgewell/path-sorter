2dae2f9c4f747e7f7d38d995b55aac1ee76b37f2
Justin Ridgewell <justin@ridgewell.name>
2020-11-01 01:38:13 -0400

    Cleanup implementation

--- src/path-sorter.ts
+++ src/path-sorter.ts
@@ -2,38 +2,39 @@ const A_GOES_FIRST = -1;
 const B_GOES_FIRST = 1;
 
 export default function pathSorter(aPath: string, bPath: string): -1 | 0 | 1 {
+  if (aPath === bPath) return 0;
+
   const aParts = aPath.split('/');
   const bParts = bPath.split('/');
+  const aLength = aParts.length;
+  const bLength = bParts.length;
+  const l = Math.max(aLength, bLength);
 
-  const l = Math.max(aParts.length, bParts.length);
   let i = 0;
   for (; i < l; i++) {
     if (aParts[i] !== bParts[i]) break;
   }
 
   // If we hit the end of one of the paths, then it's first.
-  if (aParts.length === i) return A_GOES_FIRST;
-  if (bParts.length === i) return B_GOES_FIRST;
+  if (aLength === i) return A_GOES_FIRST;
+  if (bLength === i) return B_GOES_FIRST;
+
+  const aPart = aParts[i];
+  const bPart = bParts[i];
 
   // Parent directories come after siblings and sibling-descendants.
-  if (aParts[i] === '..') return B_GOES_FIRST;
-  if (bParts[i] === '..') return A_GOES_FIRST;
+  if (aPart === '..') return B_GOES_FIRST;
+  if (bPart === '..') return A_GOES_FIRST;
 
-  // This is really to ensure that external imports will be before a `./**`.
-  if (aParts[i] === '.') return B_GOES_FIRST;
-  if (bParts[i] === '.') return A_GOES_FIRST;
+  // Self directories go after regular siblings
+  if (aPart === '.') return B_GOES_FIRST;
+  if (bPart === '.') return A_GOES_FIRST;
 
   // Prioritize siblings over sibling-descendants.
-  if (aParts.length - i === 1 && bParts.length > aParts.length) {
-    return A_GOES_FIRST;
-  }
-  if (bParts.length - i === 1 && aParts.length > bParts.length) {
-    return B_GOES_FIRST;
-  }
+  if (bLength > aLength && aLength - i === 1) return A_GOES_FIRST;
+  if (aLength > bLength && bLength - i === 1) return B_GOES_FIRST;
 
   // Finally sort by sibling
-  if (aParts[i] > bParts[i]) return B_GOES_FIRST;
-  if (bParts[i] > aParts[i]) return A_GOES_FIRST;
-
-  return 0;
+  if (aPart > bPart) return B_GOES_FIRST;
+  return A_GOES_FIRST;
 }
